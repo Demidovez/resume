38c46694fb1fc10b174e1392e4fc71bdf4653d45
Demidovez <nikolaydemidovez@example.com>
2022-12-28 15:15:25 +0300

    done avatar

---
+++ components/BlobClipAvatar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+interface IProps {
+    src: string;
+    width: number;
+    height: number;
+    alt: string;
+}
+
+const BlobClipAvatar = (props: IProps) => {
+  return <>
+    <Image
+      {...props}
+    />
+    <svg viewBox="0 0 480 480" xmlns="http://www.w3.org/2000/svg">
+      <clipPath id="svgClipAvatar"><path transform={`scale(${props.width / 480})`} fill="#474bff" d="M416,292Q439,344,393,369.5Q347,395,306,420.5Q265,446,224.5,417Q184,388,133.5,383.5Q83,379,79,330Q75,281,61.5,237Q48,193,68.5,149.5Q89,106,129,79.5Q169,53,215.5,56Q262,59,316,55.5Q370,52,379.5,107Q389,162,391,201Q393,240,416,292Z" /></clipPath>
+    </svg>
+  </>;
+};
+
+export default BlobClipAvatar;
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,26 @@
+import BlobClipAvatar from './BlobClipAvatar';
+
 import styles from '../styles/SideBar.module.scss';
 
+
 const SideBar = () => {
-  return <div className={styles.container}>SideBar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.avatar}>
+        <BlobClipAvatar 
+          src="/images/avatar.jpg" 
+          width={250}
+          height={250} 
+          alt="Фото Николай Демидовец"/>
+        <div className={styles.indicator} />
+      </div>
+      <div className={styles.contacts}>
+        <div>nikolaydemidovez@gmail.com</div>
+        <div>t.me/demidovez</div>
+      </div>
+      <button className={styles.ask_me}>Задать вопрос</button>
+    </div>
+  );
 };
 
 export default SideBar;
